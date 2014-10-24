cb9d9711ba529ff9343cfb1ffc2c460f3b643e85
Andrew Sayers <frontier-forums@pileofstuff.org>
2014-10-24 18:30:36 +0100

    Updated version number to 2.0
    Actual command: ./release.sh 2.0

--- Firefox/lib/settings.js
+++ Firefox/lib/settings.js
@@ -1,3 +1,3 @@
 exports.include = ["http://forums.frontier.co.uk/*","https://forums.frontier.co.uk/*"];
 exports.contentScriptWhen = "start";
-exports.contentScriptFile = ["BabelExt.js","jquery.min.js","jquery-observe.js","handlers.js","dispatcher.js"];
+exports.contentScriptFile = ["BabelExt.js","jquery-2.1.1.min.js","extension.js"];
